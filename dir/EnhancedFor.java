0107a3de26749a3c6b97cef2a514c9e8b59c4a20
testns2 <47989149+testns2@users.noreply.github.com>
2019-05-18 21:55:42 +0300

    Create EnhancedFor.java

---
+++ dir/EnhancedFor.java
@@ -0,0 +1,42 @@
+public class EnhancedFor
+{
+	public static void main(String[] args)
+	{	
+	}
+
+	// pre: list != null
+	// post: return sum of elements
+	// uses enhanced for loop
+	public static int sumListEnhanced(int[] list)
+	{	
+  return 0;
+	}
+
+	// pre: list != null
+	// post: return sum of elements
+	// use traditional for loop
+	public static int sumListOld(int[] list)
+	{	
+  return 0;
+	}
+
+	// pre: list != null
+	// post: none.
+	// The code appears to add one to every element in the list, but does not
+	public static void addOneError(int[] list)
+	{	
+	}
+
+	// pre: list != null
+	// post: adds one to every element of list
+	public static void addOne(int[] list)
+	{	
+	}
+
+	public static void printList(int[] list)
+	{	
+	}
+
+
+
+}
